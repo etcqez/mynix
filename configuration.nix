5f7059a5308b1f08b0dbb4927bf9610c9e253a78
etcqez <etcqez@outlook.com>
2024-06-05 02:45:37 +0800

    更新配置文件

--- configuration.nix
+++ configuration.nix
@@ -75,7 +75,6 @@
  networking.usePredictableInterfaceNames = false;
  hardware.bluetooth.enable = true; # enables support for Bluetooth
  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
- services.blueman.enable = true;
 
  # Configure network proxy if necessary
  # networking.proxy.default = "http://user:password@proxy:port/";
